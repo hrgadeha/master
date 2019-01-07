7e5f47a9f1c3d7ad257ed1fb4f5843b84423d911
Your Name <you@example.com>
2019-01-07 18:42:33 +0530

    js added

--- master/master/doctype/value_update_tool/value_update_tool.js
+++ master/master/doctype/value_update_tool/value_update_tool.js
@@ -6,3 +6,46 @@ frappe.ui.form.on('Value Update Tool', {
 
 	}
 });
+
+frappe.ui.form.on("Value Update Tool", "onload", function(frm) {
+    cur_frm.set_query("name1", function() {
+        return {
+            "filters": {
+                "docstatus": 1
+            }
+        };
+    });
+});
+
+frappe.ui.form.on("Value Update Tool", {
+    "get_data": function(frm) {
+		var temp1 = String(frm.doc.doctype)
+        	frappe.model.with_doc(temp1, frm.doc.name1, function() {
+		cur_frm.clear_table("value_update_tool_table");
+            		var tabletransfer= frappe.model.get_doc(temp1, frm.doc.name1)
+            		$.each(tabletransfer.items, function(index, row){
+                		var d = frm.add_child("value_update_tool_table");
+				d.item_name = row.item_code;
+				d.description = row.description;
+				d.qty = row.qty;
+				d.rate = row.rate;
+				d.amount = row.amount;
+                	frm.refresh_field("value_update_tool_table");
+            });
+        });
+}
+});
+
+frappe.ui.form.on("Value Update Tool", "on_submit", function(frm, doctype, name) {
+frappe.call({
+	"method": "master.master.doctype.value_update_tool.update_value",
+	args: {
+		doctype: frm.doc.doctype,
+		name: frm.doc.name1,
+		company: frm.doc.company
+     	},
+
+	callback:function(r){
+     ;}
+});
+});
